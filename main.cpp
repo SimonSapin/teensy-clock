ec5476e6fced921c5faecf1e4ecd2911cfb057a2
Simon Sapin <simon.sapin@exyr.org>
2016-08-29 10:35:47 +0200

    Move main.cpp to the top level

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include "WProgram.h"
+
+extern "C" int main(void) {
+    pinMode(13, OUTPUT);
+    while (1) {
+        digitalWriteFast(13, HIGH);
+        delay(500);
+        digitalWriteFast(13, LOW);
+        delay(500);
+    }
+}
+
