0fd8307d8ad65e47f8080fb4c75adb73157f6a6c
Simon Sapin <simon.sapin@exyr.org>
2016-08-29 11:42:19 +0200

    Integer echo over serial

--- Makefile
+++ Makefile
@@ -1,5 +1,13 @@
+SERIAL_DEVICE = /dev/ttyACM0
+
 flash: hex
-	teensy-loader-cli -w --mcu=mk20dx256 teensy3/main.hex
+	teensy-loader-cli -w -s --mcu=mk20dx256 teensy3/main.hex
 
 hex:
 	@make --no-print-directory -C teensy3 NO_ARDUINO=1
+
+cat:
+	while sleep 1; do cat $(SERIAL_DEVICE); done
+
+timestamp:
+	date +@%s > $(SERIAL_DEVICE)
--- main.cpp
+++ main.cpp
@@ -1,11 +1,41 @@
 #include "WProgram.h"
 
+
+
+uint8_t read_byte() {
+    while (!Serial.available()) {}
+    return Serial.read();
+}
+
+uint32_t read_int() {
+    uint8_t byte;
+    uint32_t result = 0;
+    for (;;) {
+        byte = read_byte();
+        if (byte >= '0' && byte <= '9') {
+            result *= 10;
+            result += byte - '0';
+        } else if (byte == '\n') {
+            return result;
+        } else {
+            return 0;
+        }
+    }
+}
+
 extern "C" int main(void) {
     pinMode(13, OUTPUT);
+    digitalWriteFast(13, HIGH);
+    delay(100);
+    digitalWriteFast(13, LOW);
+
+    Serial.begin(9600);
     while (1) {
-        digitalWriteFast(13, HIGH);
-        delay(100);
-        digitalWriteFast(13, LOW);
-        delay(500);
+        uint8_t byte;
+        byte = read_byte();
+        if (byte == '@') {
+            Serial.print("Got integer: ");
+            Serial.println(read_int());
+        }
     }
 }
