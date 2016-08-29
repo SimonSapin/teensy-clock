da6d1f5ec8e1d11dd0d7984a8053be8da1665bd8
Simon Sapin <simon.sapin@exyr.org>
2016-08-29 10:40:52 +0200

    Flash

--- Makefile
+++ Makefile
@@ -1,2 +1,5 @@
-elf:
+flash: hex
+	teensy-loader-cli -w --mcu=mk20dx256 teensy3/main.hex
+
+hex:
 	@make --no-print-directory -C teensy3 NO_ARDUINO=1
--- main.cpp
+++ main.cpp
@@ -4,9 +4,8 @@ extern "C" int main(void) {
     pinMode(13, OUTPUT);
     while (1) {
         digitalWriteFast(13, HIGH);
-        delay(500);
+        delay(100);
         digitalWriteFast(13, LOW);
         delay(500);
     }
 }
-
