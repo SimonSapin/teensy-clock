7c62d5da691023e72ae4d93059b8f542436059eb
Simon Sapin <simon.sapin@exyr.org>
2016-09-01 17:50:52 +0200

    Try (and fail) to build generated bindings.

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 #![feature(lang_items)]
 #![no_std]
 
+mod bindings;
 #[macro_use] mod serial;
 use serial::Serial;
 
@@ -9,6 +10,24 @@ pub extern fn read_int(delimiter: u8) -> u32 {
     Serial.try_read_int_until(delimiter).unwrap()
 }
 
+
+mod std {
+    pub use core::*;
+    pub mod os {
+        pub mod raw {
+            pub enum c_void {}
+            pub type c_uchar = u8;
+            pub type c_short = i16;
+            pub type c_ushort = u16;
+            pub type c_int = i32;
+            pub type c_uint = u32;
+            pub type c_long = i32;
+            pub type c_ulong = u32;
+            pub type c_longlong = i64;
+            pub type c_ulonglong = u64;
+        }
+    }
+}
 
 #[lang = "panic_fmt"]
 pub extern fn rust_begin_panic(msg: core::fmt::Arguments, file: &'static str, line: u32) -> ! {
