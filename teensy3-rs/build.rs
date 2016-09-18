6b7e0f92d1b144266f4214a3cf47eeaec3b2eb15
Simon Sapin <simon.sapin@exyr.org>
2016-09-18 12:05:39 +0200

    It runs! With the Makefile instead of gcc crate

--- teensy3-rs/build.rs
+++ teensy3-rs/build.rs
@@ -1,42 +1,12 @@
-extern crate gcc;
-extern crate glob;
-
-const MCU: &'static str = "mk20dx256";
+use std::process::Command;
 
 fn main() {
-    common(&mut gcc::Config::new(), "../teensy3/*.c")
-        .compiler("arm-none-eabi-gcc")
-        .compile("libteensyduino_c.a");
-
-    common(&mut gcc::Config::new(), "../teensy3/*.cpp")
-        .flag("-felide-constructors")
-        .flag("-fno-exceptions")
-        .flag("-fno-rtti")
-        .flag("-fkeep-inline-functions")
-        .flag("-std=gnu++0x")
-        .cpp(true)
-        .compiler("arm-none-eabi-g++")
-        .compile("libteensyduino_cpp.a");
-
-    println!("cargo:rerun-if-changed=../teensy3")
+    run("make", &["-C", "../teensy3", "NO_ARDUINO=1", "libteensy3.a"]);
+    println!("cargo:rustc-link-lib=static=teensy3");
+    println!("cargo:rustc-link-search={}/../teensy3", env!("CARGO_MANIFEST_DIR"));
+    println!("cargo:rerun-if-changed={}/../teensy3", env!("CARGO_MANIFEST_DIR"));
 }
 
-fn common<'a>(lib: &'a mut gcc::Config, pattern: &str) -> &'a mut gcc::Config {
-    for file in glob::glob(pattern).unwrap() {
-        lib.file(file.unwrap());
-    }
-    lib
-        .include("../teensy3")
-        .define("F_CPU", Some("48000000"))
-        .define("USB_SERIAL", None)
-        .define("LAYOUT_US_ENGLISH", None)
-        .define("CORE_TEENSY", None)
-        .define(&format!("__{}__", MCU.to_uppercase()), None)
-        .define("ARDUINO", Some("10600"))
-        .define("TEENSYDUINO", Some("121"))
-        .flag("-Wall")
-        .flag("-Os")
-        .flag("-mcpu=cortex-m4")
-        .flag("-mthumb")
-        .archiver("arm-none-eabi-ar")
+fn run(exe: &str, args: &[&str]) {
+    assert!(Command::new(exe).args(args).spawn().unwrap().wait().unwrap().success());
 }
